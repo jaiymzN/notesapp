ad910f9ce34a19e078e4a6153514b0a7b9e2fe54
jaiymzN <jaiymzndubuisi@gmail.com>
2024-12-15 01:20:45 -0700

    adding the bells and whistles to the notesapp

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,3 +1,4 @@
+/*
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
@@ -5,7 +6,23 @@ import { data } from './data/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
+/*
 defineBackend({
   auth,
   data,
+});
+*/
+
+import { defineBackend } from '@aws-amplify/backend';
+import { auth } from './auth/resource';
+import { data } from './data/resource';
+import { storage } from './storage/resource';
+
+/**
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ */
+defineBackend({
+  auth,
+  data,
+  storage
 });
